67d2a1fc1e642c1959b566f189b1fd577e6bfdbc
Lenon Yuri <lenonyurisilva26@gmail.com>
2023-11-14 16:50:24 -0300

    14/11/23

--- Oscar.sql
+++ Oscar.sql
@@ -4,7 +4,7 @@ USE OscarDB;
 
 
 CREATE TABLE Diretores (
-ID INT PRIMARY KEY,
+ID INT AUTO_INCREMENT PRIMARY KEY,
 Nome VARCHAR(255),
 Sexo VARCHAR(10),
 ID_Filme INT,
@@ -12,10 +12,42 @@ Tipo_de_Filme VARCHAR(255)
 );
 
 CREATE TABLE Filmes (
-ID INT PRIMARY KEY,
+ID INT AUTO_INCREMENT PRIMARY KEY,
 Nome VARCHAR(255),
 ID_Tipo_de_Filme INT,
-ID_Roteiro INT,
 Lancamento DATE
 );
 
+SELECT * FROM diretores;
+
+
+INSERT INTO
+    Diretores (
+        nome,
+        Sexo,
+        ID_Filme,
+        Tipo_de_Filme
+    )
+VALUES (
+       'Jane Campion',
+       'Feminino',
+       'Drama'
+    );
+
+
+INSERT INTO
+    Filmes (
+        nome,
+        ID_Tipo_de_Filme,
+        Lancamento
+    )
+VALUES (
+       'The Power of the Dog',
+       '',
+       '2021/12/01'
+    );
+SELECT * FROM filmes;
+
+UPDATE Diretores
+SET ID_Filme = 1
+WHERE ID = 1;
