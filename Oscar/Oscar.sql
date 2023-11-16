ad250050f8d10fb01f5f7cc27c8d9469cee15619
Lenon <lenonyurisilva26@gmail.com>
2023-11-16 17:45:05 -0300

    16/11/23

--- Oscar/Oscar.sql
+++ Oscar/Oscar.sql
@@ -2,6 +2,7 @@ CREATE DATABASE oscardb;
 
 USE oscardb;
 
+-- Tabela Diretores
 CREATE TABLE Diretores (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
@@ -9,14 +10,15 @@ CREATE TABLE Diretores (
     ID_Filme INT
 );
 
+-- Tabela Filmes
 CREATE TABLE Filmes (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Roteiro INT,
     Lancamento DATE
 );
 
+-- Tabela Atores
 CREATE TABLE Atores (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
@@ -26,176 +28,177 @@ CREATE TABLE Atores (
     Coadjuvante BOOLEAN
 );
 
+-- Tabela MelhorFilme
 CREATE TABLE MelhorFilme (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorFilme_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorDiretor
 CREATE TABLE MelhorDiretor (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Diretor INT,
-    ID_MelhorDiretor_Voto_Soma INT
+    ID_Diretor INT
 );
 
+-- Tabela MelhorAtor
 CREATE TABLE MelhorAtor (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Sexo VARCHAR(10),
-    ID_Ator INT,
-    ID_MelhorAtor_Voto_Soma INT
+    ID_Ator INT
 );
 
+-- Tabela MelhorAtriz
 CREATE TABLE MelhorAtriz (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Sexo VARCHAR(10),
-    ID_Atriz INT,
-    ID_MelhorAtriz_Voto_Soma INT
+    ID_Atriz INT
 );
 
+-- Tabela MelhorAtorCoadjuvante
 CREATE TABLE MelhorAtorCoadjuvante (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Ator INT,
-    ID_MelhorAtorCoadjuvante_Voto_Soma INT
+    ID_Ator INT
 );
 
+-- Tabela MelhorAtrizCoadjuvante
 CREATE TABLE MelhorAtrizCoadjuvante (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Atriz INT,
-    ID_MelhorAtrizCoadjuvante_Voto_Soma INT
+    ID_Atriz INT
 );
 
+-- Tabela MelhorRoteiroOriginal
 CREATE TABLE MelhorRoteiroOriginal (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorRoteiroOriginal_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorRoteiroAdaptado
 CREATE TABLE MelhorRoteiroAdaptado (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorRoteiroAdaptado_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorFilmeAnimacao
 CREATE TABLE MelhorFilmeAnimacao (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorFilmeAnimacao_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorFilmeInternacional
 CREATE TABLE MelhorFilmeInternacional (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorFilmeInternacional_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorDocumentarioLonga
 CREATE TABLE MelhorDocumentarioLonga (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorDocumentarioLonga_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorDocumentarioCurta
 CREATE TABLE MelhorDocumentarioCurta (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorDocumentarioCurta_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorCurtaLiveAction
 CREATE TABLE MelhorCurtaLiveAction (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorCurtaLiveAction_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorCurtaAnimacao
 CREATE TABLE MelhorCurtaAnimacao (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorCurtaAnimacao_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorTrilhaSonora
 CREATE TABLE MelhorTrilhaSonora (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
     Tipo_de_Filme VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorTrilhaSonora_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorCancaoOriginal
 CREATE TABLE MelhorCancaoOriginal (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorCancaoOriginal_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorSom
 CREATE TABLE MelhorSom (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorSom_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorDesignProducao
 CREATE TABLE MelhorDesignProducao (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorDesignProducao_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorFotografia
 CREATE TABLE MelhorFotografia (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorFotografia_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorMaquiagemPenteados
 CREATE TABLE MelhorMaquiagemPenteados (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorMaquiagemPenteados_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorFigurino
 CREATE TABLE MelhorFigurino (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorFigurino_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhorEdicao
 CREATE TABLE MelhorEdicao (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhorEdicao_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela MelhoresEfeitosVisuais
 CREATE TABLE MelhoresEfeitosVisuais (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
-    ID_Filme INT,
-    ID_MelhoresEfeitosVisuais_Voto_Soma INT
+    ID_Filme INT
 );
 
+-- Tabela CadastroUsuario
 CREATE TABLE CadastroUsuario (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(255),
@@ -204,32 +207,4 @@ CREATE TABLE CadastroUsuario (
     Data_de_Nascimento DATE,
     Senha VARCHAR(255),
     Usuario VARCHAR(255)
-);
-
-CREATE TABLE Votacao (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Cadastro_Usuario INT,
-    ID_MelhorFilme_Voto INT,
-    ID_MelhorDiretor_Voto INT,
-    ID_MelhorAtor_Voto INT,
-    ID_MelhorAtriz_Voto INT,
-    ID_MelhorAtorCoadjuvante_Voto INT,
-    ID_MelhorAtrizCoadjuvante_Voto INT,
-    ID_MelhorRoteiroOriginal_Voto INT,
-    ID_MelhorRoteiroAdaptado_Voto INT,
-    ID_MelhorFilmeAnimacao_Voto INT,
-    ID_MelhorFilmeInternacional_Voto INT,
-    ID_MelhorDocumentarioLonga_Voto INT,
-    ID_MelhorDocumentarioCurta_Voto INT,
-    ID_MelhorCurtaLiveAction_Voto INT,
-    ID_MelhorCurtaAnimacao_Voto INT,
-    ID_MelhorTrilhaSonora_Voto INT,
-    ID_MelhorCancaoOriginal_Voto INT,
-    ID_MelhorSom_Voto INT,
-    ID_MelhorDesignProducao_Voto INT,
-    ID_MelhorFotografia_Voto INT,
-    ID_MelhorMaquiagemPenteados_Voto INT,
-    ID_MelhorFigurino_Voto INT,
-    ID_MelhorEdicao_Voto INT,
-    ID_MelhoresEfeitosVisuais_Voto INT
 );
